25dd851f0908fe8c334514e91cea47b497f0011f
steve <sofacles@yahoo.com>
2018-06-20 19:59:45 +0300

    adding 'add project' functionality

--- Reactjs/TraversyTut/my-funky-app/src/App.js
+++ Reactjs/TraversyTut/my-funky-app/src/App.js
@@ -1,42 +1,53 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
 
-  constructor(){
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      projects : [
-        { 
-          title : "balance a tree",
-          category : "data structures and algorithms"
-        },
-        { 
-          title : "mount sash rod",
-          category : "home maintenance"
-        },
-        { 
-          title : "write a sorting algorithm",
-          category : "data structures and algorithms"
-        },
-        { 
-          title : "learn ES6",
-          category : "porfessional development"
-        }
-      ]
-    };
-  }
+        this.state = {projects: []};
+    }
 
-  render() {
-    return (
-      <div className="App">
-         My App
-         <Projects projects={this.state.projects} />
-      </div>
-    );
-  }
+    componentWillMount() {
+        this.setState({
+            projects: [
+                {
+                    title: "balance a tree",
+                    category: "data structures and algorithms"
+                },
+                {
+                    title: "mount sash rod",
+                    category: "home maintenance"
+                },
+                {
+                    title: "write a sorting algorithm",
+                    category: "data structures and algorithms"
+                },
+                {
+                    title: "learn ES6",
+                    category: "porfessional development"
+                }
+            ]})
+    }
+
+    handleAddProject(project) {
+        console.log(project);
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState(project);
+    }
+    render() {
+        return (
+            <div className="App">
+                My App
+                <Projects projects={this.state.projects} />
+                <AddProject addProject={this.handleAddProject.bind(this)}/>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ Reactjs/TraversyTut/my-funky-app/src/Components/AddProject.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+
+class AddProject extends Component {
+    static defaultProps = {
+        categories: ["home maintenance", "data structures and algorithms", "porfessional development"]
+    };
+
+    constructor() {
+        super();
+        this.state = {
+            newProject: {}
+        };
+    }
+
+    handleSubmit(e) {
+        if (this.refs.title.value === '') {
+            alert("project needs a title");
+        } else {
+            this.setState({
+                newProject: {
+                    title: this.refs.title.value,
+                    category: this.refs.category.value
+                }
+            }, function () {
+                //console.log(this.state.newProject);
+                this.props.addProject(this.state.newProject);
+                });
+        }
+        e.preventDefault();
+    }
+
+    render() {
+        let categoryOptions = this.props.categories.map(category => {
+            return <option value={category} key={category}>{category}</option>
+        });
+        return (
+            <div>
+                <h3>Add Project</h3>
+                <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div>
+                        <label>name</label><input type="text" ref="title" /><br />
+                        <label>category</label><select ref="category">{categoryOptions}</select><br />
+                        <a href="#">Add Project</a>
+                    </div>
+                    <input type="submit" value="submit" />
+                 </form>
+            </div>
+        );
+    }
+}
+
+export default AddProject;
