31471bbd6b16913b1de4023ff59327bd47e967b6
steve <sofacles@yahoo.com>
2018-07-01 12:14:28 +0200

    animating color of one bubble

---
+++ Reactjs/bubbles/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Bubble from './Components/Bubble';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Bubble></Bubble>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Reactjs/bubbles/src/Components/Bubble.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Bubble extends Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            start: null,
+            change: 0,
+            elem: null,
+            color: "#0000ff"
+        };
+    }
+
+   
+    mouseover(evt) {
+        this.setState({ elem: evt.target });
+        window.requestAnimationFrame(this.step.bind(this));
+    }
+
+    step(timestamp) {
+        const maxColor = 255;
+      
+        var progress = timestamp - this.state.start;
+        var change = this.state.change;
+        //want to go from 256 to zero in 1000 msec, well 1024, to make the math convenient.
+        change += (Math.min(progress / 4, 2));
+        var blu = Math.floor(maxColor - change);
+        var red = maxColor - blu;
+        //this.state.elem.style.fill = '#' + red.toString(16) + '00' + blu.toString(16);
+        this.setState({
+            start: this.state.start || timestamp,
+            color: '#' + red.toString(16) + '00' + blu.toString(16),
+            change: change
+        });
+        if (change <= maxColor) {
+            window.requestAnimationFrame(this.step.bind(this));
+        }
+        else {
+            console.log("Not requesting any more animation frames");
+        }
+    }
+  render() {
+    return (
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="100" cy="100" r="100" onMouseOver={this.mouseover.bind(this)} style={{ fill: this.state.color}} />
+        </svg>
+    );
+  }
+}
+
+export default Bubble;
