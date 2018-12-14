f474972b527e2495d6308e0fe5c4ad8cfd2d9ca9
Steve Degermark <=>
2018-12-14 08:17:06 -0800

    scroll works, need to trim next

--- nodeland/infiniteScroll/routes/index.js
+++ nodeland/infiniteScroll/routes/index.js
@@ -6,8 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+var fakeData = [   [{ name : "apple"},{ name : "andesite"},{ name : "ardor"},{ name : "armor"},{ name : "actress"},{ name : "alpaca"},{ name : "Antwerp"},{ name : "ambiguous"},{ name : "alteration"},{ name : "acrimony"}],
+                   [{ name : "bonobo"},{ name : "baker"},{ name : "banana"},{ name : "bun"},{ name : "borage"},{ name : "Bend"},{ name : "balloon"},{ name : "bottle"},{ name : "brake"},{ name : "break"}],  
+                   [{ name : "camber"},{ name : "cause"},{ name : "cayman"},{ name : "cereal"},{ name : "center"},{ name : "crow"},{ name : "crane"},{ name : "cone"},{ name : "core"},{ name : "cure"}],  
+                   [{ name : "day"},{ name : "denizen"},{ name : "derilect"},{ name : "detain"},{ name : "detract"},{ name : "dorsal"},{ name : "Dortmund"},{ name : "dory"},{ name : "doting"},{ name : "doyen"}],  
+                 
+];
+
 router.get('/data', function(req, res, next) {
-  res.json('[{ name : "apple"},{ name : "andesite"},{ name : "ardor"},{ name : "armor"},{ name : "actress"}]',);
+  if(!req.query.index){
+    res.json(JSON.stringify(fakeData[req.query.index]));
+  }
+  res.json(JSON.stringify(fakeData[req.query.index]));
 });
+
 
 module.exports = router;
